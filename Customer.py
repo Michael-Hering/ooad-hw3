d4ddce4be14204462523543bb5ddb2ea6fc95264
Ben Shoeman <bennieboobear@gmail.com>
2019-02-28 17:09:54 -0700

    Adding access modifiers, changed iscoming

--- Customer.py
+++ Customer.py
@@ -2,54 +2,54 @@ import random
 
 class Customer:
     def __init__(self, name):
-        self.name = name
-        self.toolsRented = []
+        self.__name = name
+        self.__toolsRented = []
 
     def pickTool(tools):
         return random.choice(tools)
     
     def rentTool(tool):
-        self.toolsRented.append(tool)
+        self.__toolsRented.append(tool)
     
     def returnTool(tool):
-        self.toolsRented.remove(tool)
+        self.__toolsRented.remove(tool)
         return tool
     
     def getNumberToolsRented():
-        return min(random.randint(self.minTools, self.maxTools), 3-len(toolsRented))
+        return min(random.randint(self.__minTools, self.__maxTools), 3-len(toolsRented))
     
     def getDaysRented():
-        return random.randint(self.minDays, self.maxDays)
+        return random.randint(self.__minDays, self.__maxDays)
 
     # Customer randomly shows up with 50% chance if store has enough tools,
     # or 0% chance if store doesn't have enough tools
     def isComing(numAvailable):
-        return numAvailable >= self.minTools and random.random() > 0.5
+        return numAvailable >= self.__minTools and len(self.__toolsRented) < 3 and random.random() > 0.5
     
     # for debug purposes, so we can print them out and see what they are
     def __repr__(self):
-        return type(self).__name__ + "('" + self.name.replace("'","\\'") + "')"
+        return type(self).__name__ + "('" + self.__name.replace("'","\\'") + "')"
 
 class CasualCustomer(Customer):
     def __init__(self, name):
         super().__init__(name)
-        self.minTools = 1
-        self.maxTools = 2
-        self.minDays = 1
-        self.maxDays = 2
+        self.__minTools = 1
+        self.__maxTools = 2
+        self.__minDays = 1
+        self.__maxDays = 2
 
 class BusinessCustomer(Customer):
     def __init__(self, name):
         super().__init__(name)
-        self.minTools = 3
-        self.maxTools = 3
-        self.minDays = 7
-        self.maxDays = 7
+        self.__minTools = 3
+        self.__maxTools = 3
+        self.__minDays = 7
+        self.__maxDays = 7
 
 class RegularCustomer(Customer):
     def __init__(self, name):
         super().__init__(name)
-        self.minTools = 1
-        self.maxTools = 3
-        self.minDays = 3
-        self.maxDays = 5
+        self.__minTools = 1
+        self.__maxTools = 3
+        self.__minDays = 3
+        self.__maxDays = 5
--- Tool.py
+++ Tool.py
@@ -1,35 +1,35 @@
 class Tool:
     def getName(self):
-        return self.name
+        return self.__name
     
     def getPricePerDay(self):
-        return self.pricePerDay
+        return self.__pricePerDay
     
     # for debug purposes, so we can print them out and see what they are
     def __repr__(self):
-        return type(self).__name__ + "('" + self.name.replace("'","\\'") + "')"
+        return type(self).__name__ + "('" + self.__name.replace("'","\\'") + "')"
 
 class PaintingTool(Tool):
     def __init__(self, name):
-        self.name = name
-        self.pricePerDay = 5.00
+        self.__name = name
+        self.__pricePerDay = 5.00
 
 class ConcreteTool(Tool):
     def __init__(self, name):
-        self.name = name
-        self.pricePerDay = 15.00
+        self.__name = name
+        self.__pricePerDay = 15.00
 
 class PlumbingTool(Tool):
     def __init__(self, name):
-        self.name = name
-        self.pricePerDay = 10.00
+        self.__name = name
+        self.__pricePerDay = 10.00
 
 class WoodworkTool(Tool):
     def __init__(self, name):
-        self.name = name
-        self.pricePerDay = 20.00
+        self.__name = name
+        self.__pricePerDay = 20.00
 
 class YardworkTool(Tool):
     def __init__(self, name):
-        self.name = name
-        self.pricePerDay = 7.50
+        self.__name = name
+        self.__pricePerDay = 7.50
